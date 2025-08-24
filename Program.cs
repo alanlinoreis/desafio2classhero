5df36aed2c33454a0f072b5c49104750483fe79d
Alan Lino dos Reis <alaanlino@gmail.com>
2025-08-24 17:28:53 -0300

    Arquivos resolucao
    Resolucao exercicio

--- Program.cs
+++ Program.cs
@@ -1,22 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-char nota;
-string notastring;
+Console.WriteLine("Informe a temperatura do usuario: ");
+double temp = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Informe uma nota: ");
-notastring = Console.ReadLine();
-nota = notastring[0];
+if (temp <= 37.5)
+    Console.WriteLine("Entrada permitida.");
+else
+    Console.WriteLine("Entrada negada por febre.");
 
-if (nota == 'A')
-    Console.WriteLine("A - Excelente");
-if (nota == 'B')
-    Console.WriteLine("B - Bom");
-if (nota == 'C')
-    Console.WriteLine("A - Regular");
-if (nota == 'D')
-    Console.WriteLine("A - Insuficiente");
-if (nota == 'F')
-    Console.WriteLine("A - Reprovado");
-
-if (nota != 'A' && nota != 'B' && nota != 'C' && nota != 'D' && nota != 'F')
-    Console.WriteLine("Nota invalida");
