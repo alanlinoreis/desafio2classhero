022ef4b46cc578c5944836e1d3aad0d670b48a7d
Alan Lino dos Reis <alaanlino@gmail.com>
2025-08-23 21:34:24 -0300

    Resolucao
    Resolucao exercicio

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+char nota;
+string notastring;
+
+Console.WriteLine("Informe uma nota: ");
+notastring = Console.ReadLine();
+nota = notastring[0];
+
+if (nota == 'A')
+    Console.WriteLine("A - Excelente");
+if (nota == 'B')
+    Console.WriteLine("B - Bom");
+if (nota == 'C')
+    Console.WriteLine("A - Regular");
+if (nota == 'D')
+    Console.WriteLine("A - Insuficiente");
+if (nota == 'F')
+    Console.WriteLine("A - Reprovado");
+
+if (nota != 'A' && nota != 'B' && nota != 'C' && nota != 'D' && nota != 'F')
+    Console.WriteLine("Nota invalida");
